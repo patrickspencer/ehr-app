0686172d60fc7d6ca65a3c46d28ea4625486bbf8
Patrick Spencer <pkspenc@gmail.com>
2026-03-08 15:59:20 -0700

    Bump version to 0.2.0

--- frontend/src/components/AppShell.tsx
+++ frontend/src/components/AppShell.tsx
@@ -34,7 +34,7 @@ function AuthGate({ children }: { children: ReactNode }) {
       <Navbar />
       <main>{children}</main>
       <div className="fixed bottom-2 left-3 text-[10px] text-gray-400">
-        v0.1.0
+        v0.2.0
       </div>
     </TabProvider>
   );
